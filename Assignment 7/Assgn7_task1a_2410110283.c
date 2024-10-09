123d060975bf91329857ed360a9bda9f4b33e940
Sabarigirish Manikandan <sabari.girish2057@gmail.com>
2024-10-09 13:53:31 +0530

    kys macros

--- Assignment 7/Assgn7_task1a_2410110283.c
+++ Assignment 7/Assgn7_task1a_2410110283.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-#define bmi(weight, height) (weight / height * height)
-
 void compute_bmi(int n, float *weight, float *height, float *bmi) {
     float w, h;
     for (int i = 0; i < n; i++) {
         w = *(weight + i);
         h = *(height + i);
-        *(bmi + i) = bmi(w, h);
+        *(bmi + i) = w / (h * h);
     }
 }
 
